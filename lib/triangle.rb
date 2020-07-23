e009c317836a743a304dd66d241f7c54860e25ba
Chindalath Traymany <chindalatht@gmail.com>
2020-07-23 04:35:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,8 +7,6 @@ class Triangle
     @s3 = s3
   end
   
-  class TriangleError < StandardError
-  end
 
   def kind
     if s1 <= 0 || s2 <= 0 || s2 <= 0
@@ -21,24 +19,9 @@ class Triangle
       :scalene
     end
   end
-  
 
-  # def kind
-  #   if side_1 > 0 && side_1 == side_2 && side_2 == side_3
-  #     :equilateral
-  #   elsif side_1 > 0 && side_1 == side_2 || side_1 == side_3 || side_2 == side_3
-  #     :isosceles
-  #   elsif side_1 > 0 && side_1 != side_2 && side_2 != side_3 && side_1 != side_3
-  #     :scalene
-  #   else
-  #     begin
-  #       raise TriangleError
-  #     rescue TriangleError => error
-  #       puts error.message
-  #     end
-  #   end
-  # end
-
+  class TriangleError < StandardError
+  end
   
 end
 
