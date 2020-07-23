8561ba3cf21c91931d0cef04deb2dfd8c7e82339
Chindalath Traymany <chindalatht@gmail.com>
2020-07-23 04:23:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end
 
   def kind
-    if side_1 == side_2 && side_2 == side_3
+    if side_1 <=0 || side_2 && side_2 == side_3
+      raise TriangleError
+    elsif side_1 == side_2 && side_2 == side_3
       :equilateral
     elsif side_2 == side_3 || side_1 == side_3 || side_1 == side_2
       :isosceles
