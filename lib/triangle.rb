029263534c74a85cf852251701861d11d9739606
Chindalath Traymany <chindalatht@gmail.com>
2020-07-23 04:44:59 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,6 @@ class Triangle
     if s1 <= 0 || s2 <= 0 || s2 <= 0 ||
       s1 + s2 < s3 ||
       s2 + s3 < s1 ||
-      s1 + s3 < s2
       raise TriangleError
     elsif s1 == s2 && s2 == s3
       :equilateral
