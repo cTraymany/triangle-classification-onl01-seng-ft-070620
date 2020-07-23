748f468b3c185ed9f163a727840e410545e29596
Chindalath Traymany <chindalatht@gmail.com>
2020-07-23 04:45:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     if s1 <= 0 || s2 <= 0 || s2 <= 0 ||
       s1 + s2 < s3 ||
-      s2 + s3 < s1 ||
+      s2 + s3 < s1
       raise TriangleError
     elsif s1 == s2 && s2 == s3
       :equilateral
